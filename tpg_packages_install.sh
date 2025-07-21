e1e9071a668e662f8a99d0359ae2149ba21b3a33
Mijail Tokarev <mijail.tokarev@ug.uchile.cl>
2025-07-21 11:14:40 +0200

    Change to a git cms-merge-topic from remote add

--- tpg_packages_install.sh
+++ tpg_packages_install.sh
@@ -2,19 +2,17 @@ tracesFilename="/afs/cern.ch/user/p/psilva/public/WaferCellMapTraces.txt"
 testFilename="/afs/cern.ch/user/m/mtokarev/public/modulelocator_trigger_test.txt"
 
 pushd $CMSSW_BASE/src
-    git cms-addpkg Geometry/HGCalMapping CondFormats/HGCalObjects DataFormats/HGCalDigi
-    git remote add mija git@github.com:MijaToka/cmssw.git 
-    git fetch mija # This is while the indexer is not added officially to CMSSW
-    git checkout -b dev/trigmapping_hgcal_up2date mija/dev/trigmapping_hgcal_up2date
-    # git cms-merge-topic -u MijaToka:dev/trigmapping_hgcal_up2date # trggmapping_hgcal_up2date
+    # git cms-addpkg Geometry/HGCalMapping CondFormats/HGCalObjects DataFormats/HGCalDigi
+    # git remote add mija git@github.com:MijaToka/cmssw.git 
+    # git fetch mija # This is while the indexer is not added officially to CMSSW
+    # git checkout -b dev/trigmapping_hgcal_up2date mija/dev/trigmapping_hgcal_up2date
+    git cms-merge-topic -u MijaToka:dev/hgcalmapping_trigger # trggmapping_hgcal_up2date
     # ^ this above would be the ideal way but it addes too many packages due to a merge which adds a lot of diffs
     
     # hgcal-comm with the module indexer chaged
     echo HGCalCommissioning >> .gitignore
-    git clone https://gitlab.cern.ch/ttravis/hgcal-comm.git HGCalCommissioning
-    pushd HGCalCommissioning
-        git checkout origin/rebased/dev-mtokarev-trigger
-    popd
+    echo Geometry/HGCalMapping/data >> .gitignore
+    git clone https://gitlab.cern.ch/ttravis/hgcal-comm.git -b rebased/dev-mtokarev-trigger HGCalCommissioning
     #update cell map
     git clone https://github.com/RSalvatico/Geometry-HGCalMapping.git Geometry/HGCalMapping/data -b CalibSurrOffsetMap
     pushd Geometry/HGCalMapping/data
